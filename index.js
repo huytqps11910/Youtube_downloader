0c92cedf4a909b554726f3d0f7629c97bf8dec1e
huytqps11910 <73825161+huytqps11910@users.noreply.github.com>
2021-02-26 02:26:11 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+const ytdl = require('ytdl-core');
+
+app.use(cors());
+
+
+app.get("/download-video", (req, res) => {
+    var URL = req.query.URL;
+    res.header('Content-Disposition', 'attachment; filename="video.mp4"');
+    ytdl(URL, { format: 'mp4' }).pipe(res);
+})
+
+app.get("/download-song", (req, res) => {
+    var URL = req.query.URL;
+    res.header('Content-Disposition', 'attachment; filename="video.mp4"');
+    ytdl(URL, { format: 'mp4' }).pipe(res);
+})
+
+app.listen(3000, () => {
+    console.log('Server works at port 3000');
+})
---
+++ script.js
@@ -0,0 +1,21 @@
+var mp4Btn = document.querySelector("#mp4-button");
+var mp3Btn = document.querySelector("#mp3-button");
+var URLinput = document.querySelector('.URL-input');
+
+mp4Btn.addEventListener("click", () => {
+    console.log(`URL: ${URLinput.value}`);
+    sendURLMP4(URLinput.value);
+})
+
+mp3Btn.addEventListener("click", () => {
+    console.log(`URL: ${URLinput.value}`);
+    sendURLMP3(URLinput.value);
+})
+
+const sendURLMP4 = (URL) => {
+    window.location.href = `http://localhost:3000/download-video?URL=${URL}`;
+}
+
+const sendURLMP3 = (URL) => {
+    window.location.href = `http://localhost:3000/download-song?URL=${URL}`;
+}
